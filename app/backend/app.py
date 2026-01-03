0615811a5a0ffaf61ba9a44f5581560bf998ea96
LMamon <knit-close.0o@icloud.com>
2026-01-02 18:05:13 -0900

    fix: added CORS middleware to allow POST requests

---
+++ app/backend/app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8001"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Query(BaseModel):
+    text:str
+
+@app.post("/query")
+async def query(q: Query):
+    return {"response": q.text}
