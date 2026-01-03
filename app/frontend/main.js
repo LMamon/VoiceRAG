a3fd85fd32a2d4662cc3dd5e631aefd70f1d08ce
LMamon <knit-close.0o@icloud.com>
2026-01-02 18:01:41 -0900

    feat: restored elapsed time, added model echo

--- app/frontend/main.js
+++ app/frontend/main.js
@@ -1,14 +1,17 @@
 import { createSTT } from "./stt.js";
 
 let isRecording = false;
+let startTime = null;
 
 const recordBtn = document.getElementById("recordBtn");
+const output = document.createElement("pre");
+document.body.appendChild(output)
 
 const stt = createSTT({
     onText: async (text) => {
         console.log("final:", text);
 
-        const res = await fetch("/query", {
+        const res = await fetch("http://localhost:8000/query", {
             method: "POST",
             headers: {"Content-Type": "application/json" },
             body: JSON.stringify({ text }),
@@ -23,13 +26,27 @@ const stt = createSTT({
 function startRecording() {
     if (isRecording) return;
     isRecording = true;
+    startTime = performance.now();
+    console.log("Recording...");
     stt.start();
 }
 
 function stopRecording() {
     if (!isRecording) return;
     isRecording = false;
+
+    const elapsed = performance.now() - startTime;
+    startTime = null;
+
+    const ms = Math.floor(elapsed % 1000);
+    const totalSeconds = Math.floor(elapsed / 1000);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
     stt.stop();
+    console.log(
+        `recording stopped after ${minutes}:${String(seconds).padStart(2, "0")}:${String(ms).padStart(3, "0")}`
+        );
 }
 
 document.addEventListener("keydown", (e) => {
