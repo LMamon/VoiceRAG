adb3d3e4b5ea703327923454fa3ce2038a6ef2da
LMamon <knit-close.0o@icloud.com>
2026-01-03 17:22:40 -0900

    feat: wired backend to local LLM runtime

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,8 +1,22 @@
+import os
+import json
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from llm_runtime import LLMRuntime
+from utils import log_interation
+
+#load env variables
+load_dotenv()
+LOG_PATH = os.getenv("LOG_PATH")
 
 app = FastAPI()
+
+llm = LLMRuntime(
+    os.getenv("MODEL_PATH"),
+    os.getenv("SYSTEM_PROMPT_PATH"),
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,7 +28,19 @@ app.add_middleware(
 
 class Query(BaseModel):
     text:str
+    duration_ms: int | None = None
 
 @app.post("/query")
 async def query(q: Query):
-    return {"response": q.text}
+    print(f"[STT] {q.duration_ms} ms | {q.text}")
+    # response = llm.generate("Explain gravity like im five.")  # eval prompt
+    response = llm.generate(q.text)
+
+    log_interation({
+        "input_text": q.text,
+        "duration_ms": q.duration_ms,
+        "response_text": response,
+        "memory_turns": len(llm.history),
+    })
+
+    return {"response": response}
