c331fcf02ac0372f32d714eab08805aa6d9f09fd
LMamon <knit-close.0o@icloud.com>
2026-01-13 17:02:52 -0900

    refactor: rebuilding using typescript for react

--- app/frontend/tts.js
+++
@@ -1,8 +0,0 @@
-export function playTTS(text) {
-    return new Promise((resolve) => {
-        const utterance = new SpeechSynthesisUtterance(text);
-        utterance.onend = resolve;
-        speechSynthesis.cancel();
-        speechSynthesis.speak(utterance);
-    })
-}
